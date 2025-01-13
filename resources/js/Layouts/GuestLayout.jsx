87005d08dacfea0d042ce7d9072a6a2c9f34e492
CesnaSusanto <revosusanto@gmail.com>
2025-01-13 20:38:11 +0700

    test push

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,18 +1,14 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import NavBar from '@/Components/NavBar';
 
 export default function GuestLayout({ children }) {
-    return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+    const navItems = [
+        { label: 'DAFTAR', path: 'register' },
+        { label: 'MASUK', path: 'login' },
+    ];
 
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
-                {children}
-            </div>
+    return (
+        <div>
+            <NavBar navItems={navItems} />
         </div>
     );
 }
---
+++ resources/js/Pages/check.jsx
@@ -0,0 +1,9 @@
+import GuestLayout from '@/Layouts/GuestLayout';
+
+export default function checking() {
+    return (
+        <GuestLayout>
+            <h1>CEK</h1>
+        </GuestLayout>
+    );
+}
