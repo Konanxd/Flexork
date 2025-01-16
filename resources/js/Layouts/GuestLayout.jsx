83b1e05ecec7abd3df818cbdea8d0a99c03b2ee0
Konanxd <iyasaggni@gmail.com>
2025-01-16 12:00:48 +0700

    fix GuestLayout

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -7,13 +7,16 @@ export default function GuestLayout({ children }) {
     ];
 
     return (
-        <div>
-            <NavBar navItems={navItems} />
-            <div className="poppins-regular flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-                <div className="mb-14 mt-14 w-full overflow-hidden border-2 border-slate-300 bg-white px-6 py-4 shadow-xl sm:max-w-lg sm:rounded-lg">
+        <>
+            <div>
+                <NavBar navItems={navItems} />
+            </div>
+
+            <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+                <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
                     {children}
                 </div>
             </div>
-        </div>
+        </>
     );
 }
